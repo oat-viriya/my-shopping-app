40520ca46cd785e6108098ab66398211ebed4ebf
Auttasit <auttasit@unixdev.co.th>
2025-06-03 18:20:02 +0700

    fix: adjust grid cols for small screen size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { ProductCard } from "../components";
 
 export default async function HomePage() {
   return (
-    <section className="auto-rows-auto min-h-fit grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 w-full">
+    <section className="auto-rows-auto min-h-fit grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 sm:gap-8 w-full">
       {productsData.map((product) => (
         <ProductCard key={product.id} productData={product} />
       ))}
