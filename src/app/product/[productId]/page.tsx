11a0c356427cd35943f0faf3ab7c668f4aa17514
Auttasit <auttasit@unixdev.co.th>
2025-06-03 10:31:37 +0700

    fix: fix params props type and page name for product details page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,12 +1,12 @@
-interface ProductPageProps {
-  params: {
-    productId: string;
-  };
+interface ProductDetailsPageProps {
+  params: Promise<{ productId: string }>;
 }
 
-export default async function ProductPage(props: ProductPageProps) {
-  const { params } = props;
-  const productId = await params.productId;
+export default async function ProductDetailsPage({
+  params,
+}: ProductDetailsPageProps) {
+  const { productId } = await params;
+
   return (
     <div className="flex flex-col w-screen h-screen items-center justify-center">
       <h1>{`This is Product details page for product id: ${productId}`}</h1>
